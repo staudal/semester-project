f2dd6009c1a9e4ec898b5ff133daf54943896eb3
staudal <jakob@staudal.dev>
2023-05-19 19:29:59 +0200

    Updated CORS filter to support DELETE methods

--- src/main/java/com/cloudrun/microservicetemplate/MicroserviceTemplateApplication.java
+++ src/main/java/com/cloudrun/microservicetemplate/MicroserviceTemplateApplication.java
@@ -21,10 +21,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /** Microservice template for Cloud Run. */
 @SpringBootApplication
 public class MicroserviceTemplateApplication {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/todos/*").allowedOrigins("*").allowedMethods("*");
+      }
+    };
+  }
+
   private static final Logger logger =
       LoggerFactory.getLogger(MicroserviceTemplateApplication.class);
 
--- src/main/java/com/cloudrun/microservicetemplate/controllers/TodoController.java
+++ src/main/java/com/cloudrun/microservicetemplate/controllers/TodoController.java
@@ -2,12 +2,14 @@ package com.cloudrun.microservicetemplate.controllers;
 
 import com.cloudrun.microservicetemplate.entities.Todo;
 import com.cloudrun.microservicetemplate.repositories.TodoRepository;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PATCH}, exposedHeaders = "Access-Control-Allow-Origin")
 @RequestMapping("/todos")
 public class TodoController {
     private final TodoRepository todoRepository;
@@ -22,14 +24,18 @@ public class TodoController {
     }
 
     @PostMapping("/new")
-    public Todo newTodo(@RequestBody Todo todo) {
+    public ResponseEntity<Todo> newTodo(@RequestBody Todo todo) {
         Todo withDate = new Todo(
                 todo.getTitle(),
                 todo.getDescription(),
                 todo.isCompleted()
         );
 
-        return todoRepository.save(withDate);
+        try {
+            return ResponseEntity.ok(todoRepository.save(withDate));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @DeleteMapping("/delete/{id}")
